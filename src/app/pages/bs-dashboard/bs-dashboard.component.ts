a66aed0c6b4a611555e57726fe9c66925616fd11
Kyrillos7 <kyrillos.nasr@flairstech.com>
2022-11-13 10:19:08 +0200

    adding dashboard page

---
+++ src/app/pages/bs-dashboard/bs-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-dashboard',
+  templateUrl: './bs-dashboard.component.html',
+  styleUrls: ['./bs-dashboard.component.scss']
+})
+export class BsDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
